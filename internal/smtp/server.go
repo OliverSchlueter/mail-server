0af0bebc8fba5ad84a013f9e617b5cf6298da927
Oliver <oschi1303@gmail.com>
2025-06-04 22:26:05 +0200

    Remove hyphens from last multiline

--- internal/smtp/server.go
+++ internal/smtp/server.go
@@ -257,9 +257,12 @@ func (s *Server) handlEhlo(session *Session, w *bufio.Writer, line string) {
 
 	// extensions
 	writeLine(w, CmdAuthLogin.Structure)
-	writeLine(w, CmdAuthPlain.Structure)
+
+	// remove "-" from last multiline
 	if s.tlsConfig != nil {
-		writeLine(w, CmdStartTls.Structure)
+		writeLine(w, strings.Replace(CmdStartTls.Structure, "-", "", -1))
+	} else {
+		writeLine(w, strings.Replace(CmdAuthPlain.Structure, "-", "", -1))
 	}
 }
 
